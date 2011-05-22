d025c3978f30ccc5e4fd057ae0c40f5f6ab508dd
Chetan Sarva <chetan@pixelcop.net>
2011-05-22 04:45:47 -0400

    UI tweaks with more users/friends

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -4,7 +4,7 @@ class Ranking < ActiveRecord::Base
   belongs_to :venue
   belongs_to :user
 
-  named_scope :recent, :order => "updated_at DESC"
+  named_scope :recent, :order => "updated_at DESC", :limit => 3
   named_scope :by_dish, :order => "dish_id ASC"
   named_scope :by_rank, :order => "position ASC"
 end
